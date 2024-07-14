5c959f199a1912a3f5f4d18b06dd97f548ada8d1
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-07-14 01:07:39 -0600

    Primer script Hello world

---
+++ 1. Hello World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpHelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
