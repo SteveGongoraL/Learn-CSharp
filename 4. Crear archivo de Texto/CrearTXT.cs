90ca5388d607e1833ae6b655f79a0ff5af84dedd
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-07-14 15:47:44 -0600

    Ejercicio para leer y crear archivos txt

---
+++ 4. Crear archivo de Texto/CrearTXT.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace CSharpGuardarTextos
+{
+    class CSharpGuardarTextos
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string rutaPath = ".\\docs\\miArchivo.txt";
+                
+                // Escribir en el TXT
+                using (StreamWriter sw = new StreamWriter(rutaPath))
+                {
+                    sw.WriteLine("Bienvenido al archivo de texto");
+                    sw.WriteLine("Aquí puedes almacenar texto fijo o dinámico usando variables");
+                    sw.Close();
+                }
+                
+                // Leer el contenido de un TXT
+                using (TextReader tw = new StreamReader(rutaPath))
+                {
+                    Console.WriteLine(tw.ReadToEnd());
+                    tw.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("El archivo se ha guardado y abrio con éxito");
+            }
+        }
+    }
+}
